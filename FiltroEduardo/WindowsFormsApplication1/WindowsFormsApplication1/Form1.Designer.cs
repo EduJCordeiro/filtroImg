ccb89d1e1bebe7c021feded18e9eccd462b1f41c
EduJCordeiro <edujcordeiro99@gmail.com>
2019-05-03 18:11:49 -0300

    filtro

---
+++ FiltroEduardo/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImagemOriginal = new System.Windows.Forms.PictureBox();
+            this.ImagemResultado = new System.Windows.Forms.PictureBox();
+            this.SelecionaImagem = new System.Windows.Forms.Button();
+            this.SalvaImagem = new System.Windows.Forms.Button();
+            this.Negativo = new System.Windows.Forms.Button();
+            this.Sepia = new System.Windows.Forms.Button();
+            this.Blur = new System.Windows.Forms.Button();
+            this.PretoeBranco = new System.Windows.Forms.Button();
+            this.BordaPreta = new System.Windows.Forms.Button();
+            this.GrayScale = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagemOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagemResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImagemOriginal
+            // 
+            this.ImagemOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ImagemOriginal.Location = new System.Drawing.Point(25, 58);
+            this.ImagemOriginal.Name = "ImagemOriginal";
+            this.ImagemOriginal.Size = new System.Drawing.Size(500, 500);
+            this.ImagemOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImagemOriginal.TabIndex = 0;
+            this.ImagemOriginal.TabStop = false;
+            this.ImagemOriginal.Click += new System.EventHandler(this.campoImagemOriginal);
+            // 
+            // ImagemResultado
+            // 
+            this.ImagemResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ImagemResultado.Location = new System.Drawing.Point(752, 58);
+            this.ImagemResultado.Name = "ImagemResultado";
+            this.ImagemResultado.Size = new System.Drawing.Size(500, 500);
+            this.ImagemResultado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImagemResultado.TabIndex = 2;
+            this.ImagemResultado.TabStop = false;
+            // 
+            // SelecionaImagem
+            // 
+            this.SelecionaImagem.BackColor = System.Drawing.Color.White;
+            this.SelecionaImagem.Location = new System.Drawing.Point(25, 577);
+            this.SelecionaImagem.Name = "SelecionaImagem";
+            this.SelecionaImagem.Size = new System.Drawing.Size(171, 30);
+            this.SelecionaImagem.TabIndex = 3;
+            this.SelecionaImagem.Text = "Selecionar Imagem";
+            this.SelecionaImagem.UseVisualStyleBackColor = false;
+            this.SelecionaImagem.Click += new System.EventHandler(this.btSeleciona);
+            // 
+            // SalvaImagem
+            // 
+            this.SalvaImagem.BackColor = System.Drawing.Color.White;
+            this.SalvaImagem.Location = new System.Drawing.Point(1081, 577);
+            this.SalvaImagem.Name = "SalvaImagem";
+            this.SalvaImagem.Size = new System.Drawing.Size(171, 30);
+            this.SalvaImagem.TabIndex = 4;
+            this.SalvaImagem.Text = "Salvar Imagem";
+            this.SalvaImagem.UseVisualStyleBackColor = false;
+            this.SalvaImagem.Click += new System.EventHandler(this.btSalvar);
+            // 
+            // Negativo
+            // 
+            this.Negativo.BackColor = System.Drawing.Color.White;
+            this.Negativo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Negativo.Location = new System.Drawing.Point(594, 197);
+            this.Negativo.Name = "Negativo";
+            this.Negativo.Size = new System.Drawing.Size(100, 30);
+            this.Negativo.TabIndex = 5;
+            this.Negativo.Text = "Negativo";
+            this.Negativo.UseVisualStyleBackColor = false;
+            this.Negativo.Click += new System.EventHandler(this.btNegativo);
+            // 
+            // Sepia
+            // 
+            this.Sepia.BackColor = System.Drawing.Color.White;
+            this.Sepia.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Sepia.Location = new System.Drawing.Point(594, 341);
+            this.Sepia.Name = "Sepia";
+            this.Sepia.Size = new System.Drawing.Size(100, 30);
+            this.Sepia.TabIndex = 6;
+            this.Sepia.Text = "Sepia";
+            this.Sepia.UseVisualStyleBackColor = false;
+            this.Sepia.Click += new System.EventHandler(this.btSepia);
+            // 
+            // Blur
+            // 
+            this.Blur.BackColor = System.Drawing.Color.White;
+            this.Blur.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Blur.Location = new System.Drawing.Point(594, 132);
+            this.Blur.Name = "Blur";
+            this.Blur.Size = new System.Drawing.Size(100, 30);
+            this.Blur.TabIndex = 7;
+            this.Blur.Text = "Blur";
+            this.Blur.UseVisualStyleBackColor = false;
+            this.Blur.Click += new System.EventHandler(this.btBlur);
+            // 
+            // PretoeBranco
+            // 
+            this.PretoeBranco.BackColor = System.Drawing.Color.White;
+            this.PretoeBranco.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PretoeBranco.Location = new System.Drawing.Point(594, 481);
+            this.PretoeBranco.Name = "PretoeBranco";
+            this.PretoeBranco.Size = new System.Drawing.Size(100, 30);
+            this.PretoeBranco.TabIndex = 8;
+            this.PretoeBranco.Text = "Preto e branco";
+            this.PretoeBranco.UseVisualStyleBackColor = false;
+            this.PretoeBranco.Click += new System.EventHandler(this.btPretoBranco);
+            // 
+            // BordaPreta
+            // 
+            this.BordaPreta.BackColor = System.Drawing.Color.White;
+            this.BordaPreta.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BordaPreta.Location = new System.Drawing.Point(594, 411);
+            this.BordaPreta.Name = "BordaPreta";
+            this.BordaPreta.Size = new System.Drawing.Size(100, 30);
+            this.BordaPreta.TabIndex = 14;
+            this.BordaPreta.Text = "Borda preta";
+            this.BordaPreta.UseVisualStyleBackColor = false;
+            this.BordaPreta.Click += new System.EventHandler(this.btBordaPreta);
+            // 
+            // GrayScale
+            // 
+            this.GrayScale.BackColor = System.Drawing.Color.White;
+            this.GrayScale.Location = new System.Drawing.Point(594, 272);
+            this.GrayScale.Name = "GrayScale";
+            this.GrayScale.Size = new System.Drawing.Size(100, 30);
+            this.GrayScale.TabIndex = 27;
+            this.GrayScale.Text = "Gray-Scale";
+            this.GrayScale.UseVisualStyleBackColor = false;
+            this.GrayScale.Click += new System.EventHandler(this.btGrayscale);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(126, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 39);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Imagem Original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(919, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 39);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Resultado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(604, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 29);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Filtros";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1264, 619);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GrayScale);
+            this.Controls.Add(this.BordaPreta);
+            this.Controls.Add(this.PretoeBranco);
+            this.Controls.Add(this.Blur);
+            this.Controls.Add(this.Sepia);
+            this.Controls.Add(this.Negativo);
+            this.Controls.Add(this.SalvaImagem);
+            this.Controls.Add(this.SelecionaImagem);
+            this.Controls.Add(this.ImagemResultado);
+            this.Controls.Add(this.ImagemOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ImagemOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagemResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImagemOriginal;
+        private System.Windows.Forms.PictureBox ImagemResultado;
+        private System.Windows.Forms.Button SelecionaImagem;
+        private System.Windows.Forms.Button SalvaImagem;
+        private System.Windows.Forms.Button Negativo;
+        private System.Windows.Forms.Button Sepia;
+        private System.Windows.Forms.Button Blur;
+        private System.Windows.Forms.Button PretoeBranco;
+        private System.Windows.Forms.Button BordaPreta;
+        private System.Windows.Forms.Button GrayScale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FiltroEduardo/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        
+
+
+
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btSeleciona(object sender, EventArgs e)
+        {
+            OpenFileDialog caixa = new OpenFileDialog();
+            Bitmap Imagem;
+            // caixa.Filter = "Imagem JPG(.jpg,.bmp,.gif, .png, .tif)|*.jpg ||*.bmp|| *gif|| *png|";
+            caixa.Filter = "Imagem JPG(.jpg)|*.jpg|Imagme bmp(.bmp)|*.bmp|Imagem Gif(.Gif)|*gif|Imagem Png(.png)|*png|Imagem TIFF(.tif)|*tif";
+            caixa.InitialDirectory = @"C:\Users\'\Desktop\Imagem Trabalho";
+            caixa.Title = "Selecionar Imagem";
+            if (caixa.ShowDialog() == DialogResult.OK)
+            {
+                Imagem = new Bitmap(Image.FromFile(caixa.FileName));
+                ImagemOriginal.Image = Imagem;
+            }
+        }
+
+        private void btSalvar(object sender, EventArgs e)
+        {
+            SaveFileDialog caixa = new SaveFileDialog();
+            caixa.Filter = "Imagem jpeg(.jpg)|*jpg";
+            caixa.DefaultExt = "jpg";
+            caixa.Title = "Salvar Imagem";
+            if (DialogResult.OK == caixa.ShowDialog())
+            {
+                string nome = caixa.FileName;
+                Bitmap NovaImagem = new Bitmap(ImagemResultado.Image);
+                NovaImagem.Save(nome, System.Drawing.Imaging.ImageFormat.Png);
+                
+            }
+
+        }
+
+
+        private void btNegativo(object sender, EventArgs e)
+        {
+            Bitmap original = new Bitmap(ImagemOriginal.Image);
+            Bitmap NovaImagem = new Bitmap(original.Width, original.Height);
+            for (int i = 0; i < original.Width; i++)
+            {
+                for (int j = 0; j < original.Height; j++)
+                {
+                    Color originalColor = original.GetPixel(i, j);//pega a cor original
+                    Color CorEmNegativo = Color.FromArgb(255 - originalColor.R, 255 - originalColor.G, 255 - originalColor.B); // transforma em negativo
+
+                    NovaImagem.SetPixel(i, j, CorEmNegativo);
+
+
+                }
+            }
+            ImagemResultado.Image = NovaImagem;
+        }
+
+        private void btSepia(object sender, EventArgs e)
+        {
+            Bitmap original = new Bitmap(ImagemOriginal.Image);
+            Bitmap NovaImagem = new Bitmap(original.Width, original.Height);
+
+            //pega a dimensão da imagem
+            int width = original.Width;
+            int height = original.Height;
+
+            //cor do pixel
+            Color p;
+
+            //sepia
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    p = original.GetPixel(x, y);
+
+                    
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+
+                    
+                    int tr = (int)(0.393 * r + 0.769 * g + 0.189 * b);
+                    int tg = (int)(0.349 * r + 0.686 * g + 0.168 * b);
+                    int tb = (int)(0.272 * r + 0.534 * g + 0.131 * b);
+
+                    
+                    if (tr > 255)
+                    {
+                        r = 255;
+                    }
+                    else
+                    {
+                        r = tr;
+                    }
+
+                    if (tg > 255)
+                    {
+                        g = 255;
+                    }
+                    else
+                    {
+                        g = tg;
+                    }
+
+                    if (tb > 255)
+                    {
+                        b = 255;
+                    }
+                    else
+                    {
+                        b = tb;
+                    }
+
+                    
+                    NovaImagem.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                }
+            }
+
+   
+
+
+            ImagemResultado.Image = NovaImagem;
+
+        }
+
+
+        private void btBlur(object sender, EventArgs e)
+        {
+            Bitmap Imagem = new Bitmap(ImagemOriginal.Image);
+            int Largura = Imagem.Width;
+            int Altura = Imagem.Height;
+            int pixelR = 0;
+            int pixelG = 0;
+            int pixelB = 0;
+            int pixelA = 0;
+            int i, j;
+            int[] todosPixeisR = new int[9];
+            int[] todosPixeisG = new int[9];
+            int[] todosPixeisB = new int[9];
+            int[] todosPixeisA = new int[9];
+            Bitmap NovaImagem = new Bitmap(Largura, Altura);
+            
+            for (i = 1; i < Largura - 1; i++)
+            {
+                for (j = 1; j < Altura - 1; j++)
+                {
+                    pixelR = (
+                             Imagem.GetPixel(i - 1, j - 1).R +
+                             Imagem.GetPixel(i - 1, j).R +
+                             Imagem.GetPixel(i - 1, j + 1).R +
+
+                             Imagem.GetPixel(i, j - 1).R +
+                             Imagem.GetPixel(i, j).R +
+                             Imagem.GetPixel(i, j + 1).R +
+
+                             Imagem.GetPixel(i + 1, j - 1).R + 
+                             Imagem.GetPixel(i + 1, j).R +
+                             Imagem.GetPixel(i + 1, j + 1).R
+                             ) / 9;
+
+                    pixelG = (
+                             Imagem.GetPixel(i - 1, j - 1).G +
+                             Imagem.GetPixel(i - 1, j).G +
+                             Imagem.GetPixel(i - 1, j + 1).G +
+
+                             Imagem.GetPixel(i, j - 1).G +
+                             Imagem.GetPixel(i, j).G +
+                             Imagem.GetPixel(i, j + 1).G +
+
+                             Imagem.GetPixel(i + 1, j - 1).G + 
+                             Imagem.GetPixel(i + 1, j).G +
+                             Imagem.GetPixel(i + 1, j + 1).G
+                             ) / 9;
+
+                    pixelB = (
+                              Imagem.GetPixel(i - 1, j - 1).B +
+                              Imagem.GetPixel(i - 1, j).B +
+                              Imagem.GetPixel(i - 1, j + 1).B +
+
+                              Imagem.GetPixel(i, j - 1).B +
+                              Imagem.GetPixel(i, j).B +
+                              Imagem.GetPixel(i, j + 1).B +
+
+                              Imagem.GetPixel(i + 1, j - 1).B + 
+                              Imagem.GetPixel(i + 1, j).B +
+                              Imagem.GetPixel(i + 1, j + 1).B
+                              ) / 9;
+
+                    pixelA = (
+                             Imagem.GetPixel(i - 1, j - 1).A +
+                             Imagem.GetPixel(i - 1, j).A +
+                             Imagem.GetPixel(i - 1, j + 1).A +
+
+                             Imagem.GetPixel(i, j - 1).A +
+                             Imagem.GetPixel(i, j).A +
+                             Imagem.GetPixel(i, j + 1).A +
+
+                             Imagem.GetPixel(i + 1, j - 1).A + 
+                             Imagem.GetPixel(i + 1, j).A +
+                             Imagem.GetPixel(i + 1, j + 1).A
+                            ) / 9;
+
+
+                    NovaImagem.SetPixel(i, j, Color.FromArgb(pixelA, pixelR, pixelG, pixelB));
+
+                }
+            }
+
+            ImagemResultado.Image = NovaImagem;
+        }
+
+        private void btPretoBranco(object sender, EventArgs e) 
+        {
+            Bitmap original = new Bitmap(ImagemOriginal.Image);
+            Bitmap NovaImagem = new Bitmap(original.Width, original.Height);
+
+            //get image dimension
+            int width = original.Width;
+            int height = original.Height;
+
+            for(int i =0; i< original.Height; i++)
+            {
+                for(int j = 0; j< original.Width; j++)
+                {
+                    if (original.GetPixel(j, i).R >= 128 && original.GetPixel(j, i).G >= 128 && original.GetPixel(j, i).B >= 128)
+                        NovaImagem.SetPixel(j, i, Color.FromArgb(255, 255, 255));
+                    else
+                        NovaImagem.SetPixel(j, i, Color.FromArgb(0, 0, 0));
+                }
+            }
+            ImagemResultado.Image = NovaImagem;
+
+        }
+
+        
+
+
+       
+
+        private void btBordaPreta(object sender, EventArgs e)
+        {
+            Bitmap Imagem = new Bitmap(ImagemOriginal.Image);
+            int Largura = Imagem.Width;
+            int Altura = Imagem.Height;
+            int[,] Prewittx = new int[,] { { -1, -1, -1 }, { 0, 0, 0 }, { 1, 1, 1 } };
+            int[,] Prewitty = new int[,] { { -1, 0, 1 }, { -1, 0, 1 }, { -1, 0, 1 } };
+            int[,] todosPixeisR = new int[Largura, Altura];
+            int[,] todosPixeisG = new int[Largura, Altura];
+            int[,] todosPixeisB = new int[Largura, Altura];
+            Bitmap NovaImagem = new Bitmap(Largura, Altura); //para trabalhar com a imagem, novo bmp com o mesmo tamanho da img original
+            int limite = 128 * 128;
+            for (int i = 0; i < Largura; i++)
+            {
+                for (int j = 0; j < Altura; j++)
+                {
+                    todosPixeisR[i, j] = Imagem.GetPixel(i, j).R;
+                    todosPixeisG[i, j] = Imagem.GetPixel(i, j).G;
+                    todosPixeisB[i, j] = Imagem.GetPixel(i, j).B;
+                }
+            }
+            int novo_rx, novo_ry, novo_gx, novo_gy, novo_bx, novo_by;//, rc, gc, bc;
+            for (int i = 1; i < Imagem.Width - 1; i++)
+            {
+                for (int j = 1; j < Imagem.Height - 1; j++)
+                {
+                    novo_rx = 0;
+                    novo_ry = 0;
+                    novo_gx = 0;
+                    novo_gy = 0;
+                    novo_bx = 0;
+                    novo_by = 0;
+                    for (int x = -1; x < 2; x++)
+                    {
+                        for (int y = -1; y < 2; y++)
+                        {
+                            novo_rx += Prewittx[x + 1, y + 1] * todosPixeisR[i + y, j + x];
+                            novo_ry += Prewitty[x + 1, y + 1] * todosPixeisR[i + y, j + x];
+                            novo_gx += Prewittx[x + 1, y + 1] * todosPixeisG[i + y, j + x];
+                            novo_gy += Prewitty[x + 1, y + 1] * todosPixeisG[i + y, j + x];
+                            novo_bx += Prewittx[x + 1, y + 1] * todosPixeisB[i + y, j + x];
+                            novo_by += Prewitty[x + 1, y + 1] * todosPixeisB[i + y, j + x];
+                        }
+                    }
+                    if (
+                        novo_rx * novo_rx + novo_ry * novo_ry > limite ||
+                        novo_gx * novo_gx + novo_gy * novo_gy > limite ||
+                        novo_bx * novo_bx + novo_by * novo_by > limite
+                        )
+                        NovaImagem.SetPixel(i, j, Color.Black);
+                    else
+                        NovaImagem.SetPixel(i, j, Color.Transparent);
+                }
+            }
+            ImagemResultado.Image = NovaImagem;
+        }
+
+
+       
+
+
+        
+
+
+        private void btGrayscale(object sender, EventArgs e)
+        {
+            Bitmap Imagem = new Bitmap(ImagemOriginal.Image);
+
+            int todospixelsR;
+            int todospixelsG;
+            int todospixelsB;
+            int Largura = Imagem.Width;
+            int Altura = Imagem.Height;
+            int cinza;
+            Bitmap NovaImagem = new Bitmap(Largura, Altura);
+            for (int x = 0; x < Largura; x++)
+            {
+                for (int y = 0; y < Altura; y++)
+                {
+                    todospixelsR = Imagem.GetPixel(x, y).R;
+                    todospixelsG = Imagem.GetPixel(x, y).G;
+                    todospixelsB = Imagem.GetPixel(x, y).B;
+                    cinza = (todospixelsR + todospixelsG + todospixelsB) / 3;
+                    NovaImagem.SetPixel(x, y, Color.FromArgb(255, cinza, cinza, cinza));
+                }
+            }
+            ImagemResultado.Image = NovaImagem;
+        }
+
+        private void campoImagemOriginal(object sender, EventArgs e)
+        {
+
+        }
+
+
+    
+    }
+    }
+
+    
